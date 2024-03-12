f37167f213520ec5c1fc5f2955c6aa0db6eef9ce
Rutuja Bahadure <bahadurerutu558@gamil.com>
2024-03-12 17:52:25 +0530

    fronted added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config({ path: ".env" })
 const app = express()
 
 //middlewares
-app.use(express.json)
+app.use(express.json())
 app.use(express.static("dist"))
 app.use(cors({
     origin: process.env.FRONTEND_URL
@@ -24,14 +24,14 @@ app.use("*", (req, res) => {
 
 //error
 
-app.use((err,req,res,next)=>{
-    res.status(500).json({message: err.message || "something went wrong"})
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: err.message || "something went wrong" })
 })
 //db
 
 mongoose.connect(process.env.MONGO_URL)
 
-mongoose.connection.once("open", ()=> {
+mongoose.connection.once("open", () => {
     console.log("MONGO CONNECTED")
     app.listen(process.env.PORT, console.log("SERVER RUNNING"))
 })
